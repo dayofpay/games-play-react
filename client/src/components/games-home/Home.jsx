36ca1af7c344c6faa6f367f7773f78612bc109aa
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-11-21 09:48:32 +0200

    Home page setup

--- client/src/components/games-home/Home.jsx
+++ client/src/components/games-home/Home.jsx
@@ -1,57 +1,55 @@
-export default function Home(){
-    return (
-        <section id="welcome-world">
+import React, { useEffect, useState } from "react";
+import { getAll } from "../../services/game-services";
 
-        <div className="welcome-message">
-            <h2>ALL new games are</h2>
-            <h3>Only in GamesPlay</h3>
-        </div>
-        <img src="./images/four_slider_img01.png" alt="hero"/>
+export default function Home() {
+  const [games, setGames] = useState([]);
 
-        <div id="home-page">
-            <h1>Latest Games</h1>
+  useEffect(() => {
+    getAll()
+      .then((response) => {
+        setGames(response);
+      })
+      .catch((err) => {
 
+      });
+  }, []);
 
-            <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/CoverFire.png"/>
-                </div>
-                <h3>Cover Fire</h3>
-                <div className="rating">
-                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                </div>
-                <div className="data-buttons">
-                    <a href="#" className="btn details-btn">Details</a>
-                </div>
+  return (
+    <section id="welcome-world">
+      <div className="welcome-message">
+        <h2>ALL new games are</h2>
+        <h3>Only in GamesPlay</h3>
+      </div>
+      <img src="./images/four_slider_img01.png" alt="hero" />
+
+      <div id="home-page">
+        <h1>Latest Games</h1>
+
+        {games.length > 0 ? (
+          games.map((game, index) => (
+            <div className="game" key={index}>
+              <div className="image-wrap">
+                <img src={game.imageUrl} alt={game.title} />
+              </div>
+              <h3>{game.title}</h3>
+              <div className="rating">
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+                <span>☆</span>
+              </div>
+              <div className="data-buttons">
+                <a href="/game-details/:id" className="btn details-btn">
+                  Details
+                </a>
+              </div>
             </div>
-            <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/ZombieLang.png"/>
-                </div>
-                <h3>Zombie Lang</h3>
-                <div className="rating">
-                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                </div>
-                <div className="data-buttons">
-                    <a href="#" className="btn details-btn">Details</a>
-                </div>
-            </div>
-            <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/MineCraft.png"/>
-                </div>
-                <h3>MineCraft</h3>
-                <div className="rating">
-                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                </div>
-                <div className="data-buttons">
-                    <a href="#" className="btn details-btn">Details</a>
-                </div>
-            </div>
-
-
-            <p className="no-articles">No games yet</p>
-        </div>
+          ))
+        ) : (
+          <p className="no-articles">No games yet</p>
+        )}
+      </div>
     </section>
-    )
+  );
 }
