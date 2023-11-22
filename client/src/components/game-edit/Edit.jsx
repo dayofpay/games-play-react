d85775d1898f2efb0729982f4fe4b49ad0b535d5
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-11-22 11:02:51 +0200

    Remove unused state

--- client/src/components/game-edit/Edit.jsx
+++ client/src/components/game-edit/Edit.jsx
@@ -47,7 +47,6 @@ export default function Edit() {
     imageUrl: "",
     summary: "",
   });
-  const [hasError, setErrorState] = useState(false);
   const FORM_DATA = {
     title: "",
     category: "",
